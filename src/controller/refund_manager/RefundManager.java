ae2cee4fa974ca2ce19107f4752286266deb6047
Adrian <aleyvaschz@gmail.com>
2017-10-25 21:48:42 -0500

    Implement refund manager

--- src/controller/refund_manager/RefundManager.java
+++ src/controller/refund_manager/RefundManager.java
@@ -3,22 +3,40 @@ package controller.refund_manager;
 import controller.BaseController;
 import model.Show;
 import model.Ticket;
+import utils.MessageBack;
 
 import java.util.ArrayList;
 
 public class RefundManager extends BaseController implements RefundManagerContract{
+    public static final int FIRST_CONDITION = 1;
+    public static final int SECOND_CONDITION = 2;
+    public static final int THIRD_CONDITION = 3;
+
     @Override
     protected void setupDependencies() {
-        
-    }
-
-    @Override
-    public void refundByShowCancellation(Show show) {
 
     }
 
     @Override
-    public void refundByCustomerCancellation(ArrayList<Ticket> tickets, int typeOfCondition) {
+    public MessageBack refundByShowCancellation(Show show) {
+        /*
+         * Insert postgresql query
+         */
+        return new MessageBack();
+    }
 
+    @Override
+    public MessageBack refundByCustomerCancellation(ArrayList<Ticket> tickets, int typeOfCondition) {
+        switch (typeOfCondition){
+            case FIRST_CONDITION:
+                break;
+            case SECOND_CONDITION:
+                break;
+            case THIRD_CONDITION:
+                break;
+            default:
+                break;
+        }
+        return new MessageBack();
     }
 }
--- src/controller/refund_manager/RefundManagerContract.java
+++ src/controller/refund_manager/RefundManagerContract.java
@@ -2,10 +2,11 @@ package controller.refund_manager;
 
 import model.Show;
 import model.Ticket;
+import utils.MessageBack;
 
 import java.util.ArrayList;
 
 public interface RefundManagerContract {
-    void refundByShowCancellation(Show show);
-    void refundByCustomerCancellation(ArrayList<Ticket> tickets, int typeOfCondition);
+    MessageBack refundByShowCancellation(Show show);
+    MessageBack refundByCustomerCancellation(ArrayList<Ticket> tickets, int typeOfCondition);
 }
