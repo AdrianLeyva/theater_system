64fee26eba12134b6e27ca47688e296ed50afa94
Adrian <aleyvaschz@gmail.com>
2017-11-15 14:49:52 -0600

    Fix call to controller

--- src/controller/show_manager/reschedule/Reschedule.java
+++ src/controller/show_manager/reschedule/Reschedule.java
@@ -2,10 +2,15 @@ package controller.show_manager.reschedule;
 
 import controller.BaseController;
 import model.Show;
+import model.persistence.Shows;
+import model.persistence.dao.ShowDaoImpl;
 import utils.DateParser;
 
+import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * This class handles all reschedule processes of show manager module.
  * @author Adrián Leyva Sánchez
@@ -17,23 +22,37 @@ public class Reschedule extends BaseController implements RescheduleProcess{
     }
 
     @Override
-    public void rescheduleShow(Show show) {
-         /*
-            Insert postgresql query
-         */
-
+    public void rescheduleShow(Shows show) {
+        ShowDaoImpl dao = new ShowDaoImpl();
+        try {
+            dao.modify(show);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void rescheduleAllShows(ArrayList<Show> shows, ArrayList<Date> dates) {
-        for(int i=0; i<shows.size(); i++){
-            String newDate = DateParser.parseDateTimeFormat(DateParser.DATE_PATTERN, dates.get(i));
-            shows.get(i).setDate(newDate);
-        }
+    public void rescheduleAllShows(ArrayList<Shows> shows, ArrayList<Date> dates, DefaultTableModel model) {
+        ShowDaoImpl dao = new ShowDaoImpl();
 
-        /*
-            Insert postgresql query
-         */
+        for(int i = 0; i < model.getRowCount(); i++){
+            java.util.Date date = new java.util.Date();
+            String showDate = model.getValueAt(i,1).toString();
+            String[] splitsDate = showDate.split("/");
+
+            System.out.println(showDate);
+            date = new GregorianCalendar(Integer.valueOf(splitsDate[0]), Integer.valueOf(splitsDate[1]),
+                    Integer.valueOf(splitsDate[2])).getTime();
+
+            shows.get(i).setDate(date);
+            shows.get(i).setSchedule(model.getValueAt(i,2).toString());
+
+            try {
+                dao.modify(shows.get(i));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
